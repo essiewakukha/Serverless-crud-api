47011204ca3446df077dbf3b97b7d40f380654f3
Egbogu Chinedu Joseph <137600196+essiewakukha@users.noreply.github.com>
2026-09-22 21:59:46 +0300

    initial dynamodb.tf commit

---
+++ terraform/dynamodb.tf
@@ -0,0 +1,17 @@
+resource "aws_dynamodb_table" "this" {
+  name             = "${var.project_name}-${var.dynamodb_table_name}"
+  hash_key         = "id" 
+  billing_mode     = "PAY_PER_REQUEST" #on demand capacity
+
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Project = var.project_name
+    Environment = var.environments
+  }
+
+}
